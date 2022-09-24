59d4a782162259e1d3d821ee24c18f68d812eff6
jcarlos21 <j.carlos2020@live.com>
2022-09-24 00:11:14 -0300

    Configurando entrega_01

--- teste_1.m
+++ teste_1.m
@@ -1,25 +1,22 @@
-% for dRrange = 1000:10:12000
-%     disp(dRrange)
-% end
-% 
-% for dRrange = 1000:2000:12000
-%    disp(dRrange)
-% end
-
-
-% Primeiro código de teste
+clear; clc; close all;                                         % Limpa variáveis, limpa tela e fecha todas as figuras
 
 vtFc = [800 900 1800 1900 2100];
+dR = [12e3 12e3 12e3 12e3 12e3];                               % Raio máximo da celula
+dOutRate = [12 12 12 12 12];                                   % Condição para entrar no laço While
+
+
 for iFc = 1:length(vtFc)
-    for dR = 1000:10:12000
-        dFc = vtFc(iFc);
+    dFc = vtFc(iFc);
+
+    while (dOutRate(iFc)>10)
+
         % Entrada de parâmetros
         % Cálculos de outras variáveis que dependem dos parâmetros de entrada
-        dPasso = ceil(dR/50);                                      % Resolução do grid: distância entre pontos de medição
+        dPasso = ceil(dR(iFc)/50);                                 % Resolução do grid: distância entre pontos de medição
         dRMin = dPasso;                                            % Raio de segurança
-        dIntersiteDistance = 2*sqrt(3/4)*dR;                       % Distância entre ERBs (somente para informação)
-        dDimX = 5*dR;                                              % Dimensão X do grid
-        dDimY = 6*sqrt(3/4)*dR;                                    % Dimensão Y do grid
+        dIntersiteDistance = 2*sqrt(3/4)*dR(iFc);                  % Distância entre ERBs (somente para informação)
+        dDimX = 5*dR(iFc);                                         % Dimensão X do grid
+        dDimY = 6*sqrt(3/4)*dR(iFc);                               % Dimensão Y do grid
         dPtdBm = 57;                                               % EIRP (incluindo ganho e perdas) (https://pt.slideshare.net/naveenjakhar12/gsm-link-budget)
         dPtLinear = 10^(dPtdBm/10)*1e-3;                           % EIRP em escala linear
         dSensitivity = -104;                                       % Sensibilidade do receptor (http://www.comlab.hut.fi/opetus/260/1v153.pdf)
@@ -31,9 +28,9 @@ for iFc = 1:length(vtFc)
         vtBs = [ 0 ];
         dOffset = pi/6;
         for iBs = 2 : 7
-            vtBs = [ vtBs dR*sqrt(3)*exp( j * ( (iBs-2)*pi/3 + dOffset ) ) ];
+            vtBs = [ vtBs dR(iFc)*sqrt(3)*exp( j * ( (iBs-2)*pi/3 + dOffset ) ) ];
         end
-        vtBs = vtBs + (dDimX/2 + j*dDimY/2);                        % Ajuste de posição das bases (posição relativa ao canto inferior esquerdo)
+        vtBs = vtBs + (dDimX/2 + j*dDimY/2);                       % Ajuste de posição das bases (posição relativa ao canto inferior esquerdo)
         %
         % Matriz de referência com posição de cada ponto do grid (posição relativa ao canto inferior esquerdo)
         dDimY = ceil(dDimY+mod(dDimY,dPasso));                     % Ajuste de dimensão para medir toda a dimensão do grid
@@ -44,25 +41,29 @@ for iFc = 1:length(vtFc)
         % medido. Essa potência é a maior entre as 7 ERBs.
         mtPowerFinaldBm = -inf*ones(size(mtPosy));
         % Calcular O REM de cada ERB e aculumar a maior potência em cada ponto de medição
-        for iBsD = 1 : length(vtBs)                                 % Loop nas 7 ERBs
+        for iBsD = 1 : length(vtBs)                                % Loop nas 7 ERBs
             % Matriz 3D com os pontos de medição de cada ERB. Os pontos são
             % modelados como números complexos X +jY, sendo X a posição na abcissa e Y, a posição no eixo das ordenadas
             mtPosEachBS =(mtPosx + j*mtPosy)-(vtBs(iBsD));
-            mtDistEachBs = abs(mtPosEachBS);              % Distância entre cada ponto de medição e a sua ERB
-            mtDistEachBs(mtDistEachBs < dRMin) = dRMin;             % Implementação do raio de segurança
+            mtDistEachBs = abs(mtPosEachBS);                       % Distância entre cada ponto de medição e a sua ERB
+            mtDistEachBs(mtDistEachBs < dRMin) = dRMin;            % Implementação do raio de segurança
             % Okumura-Hata (cidade urbana) - dB
             mtPldB = 69.55 + 26.16*log10(dFc) + (44.9 - 6.55*log10(dHBs))*log10(mtDistEachBs/1e3) - 13.82*log10(dHBs) - dAhm;
-            mtPowerEachBSdBm = dPtdBm - mtPldB;           % Potências recebidas em cada ponto de medição
+            mtPowerEachBSdBm = dPtdBm - mtPldB;                     % Potências recebidas em cada ponto de medição
             % Cálulo da maior potência em cada ponto de medição
             mtPowerFinaldBm = max(mtPowerFinaldBm,mtPowerEachBSdBm);
         end
         %
         % Outage (limite 10%)
-        dOutRate = 100*length(find(mtPowerFinaldBm < dSensitivity))/numel(mtPowerFinaldBm);
-        if (dOutRate <= 10)
-            disp(['Frequência da portadora = ' num2str(dFc)]);
-            disp(['Taxa de outage = ' num2str(dOutRate) ' %']);
-            disp(['Raio celular aproximado = ' num2str(dR)])
+        dOutRate(iFc) = 100*length(find(mtPowerFinaldBm < dSensitivity))/numel(mtPowerFinaldBm);
+
+        if (dOutRate(iFc) > 10)
+            dR(iFc) = dR(iFc)-10;
         end
     end
+
+    disp(['Frequência da portadora = ' num2str(dFc)]);
+    disp(['Taxa de outage = ' num2str(dOutRate(iFc)) ' %']);
+    disp(['Raio celular aproximado = ' num2str(dR(iFc))])
+    disp(['-------------------------------'])
 end
